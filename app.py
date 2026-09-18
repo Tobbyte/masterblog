0c313247e4562ae1f346af7e168f8074a3134012
Tobbyte <7722596+Tobbyte@users.noreply.github.com>
2026-09-18 11:37:17 +0200

    add post, route_add,

--- app.py
+++ app.py
@@ -1,10 +1,11 @@
 """A simple Flask app for a blog."""
 
 import json
+from copy import deepcopy
 from pathlib import Path
 from typing import Any
 
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 
 class Masterblog:
@@ -13,17 +14,53 @@ class Masterblog:
     def __init__(self) -> None:
         """Initialize the Flask app."""
         self.app = Flask(__name__)
-        self.app.add_url_rule("/", view_func=self.index)
+        self.app.add_url_rule("/", view_func=self.route_index)
+        self.app.add_url_rule(
+            "/add",
+            view_func=self.route_add,
+            methods=["GET", "POST"],
+        )
 
-    def index(self) -> str:
+        self.load_data()
+
+    def route_index(self) -> str:
         """Render the index page with blog posts."""
-        with Path("data/database.json").open(encoding="utf-8") as f:
-            blog_posts = json.load(f)
         return render_template(
             "index.html",
-            posts=blog_posts,
+            posts=self.blog_posts,
             blogtitle="Mein Blog",
         )
+
+    def route_add(self) -> str:
+        """Render form on get or save post on post."""
+        if request.method == "POST":
+            self.save_data(request.form.to_dict(flat=False))
+
+        return render_template("add.html")
+
+    def save_data(self, new_post: dict) -> None:
+        """Save a new blog post to the JSON file."""
+        new_id = len(self.blog_posts) + 1
+        new_post["id"] = new_id
+        posts_copy = deepcopy(self.blog_posts)
+        posts_copy.append(new_post)
+        try:
+            with Path("data/database.json").open("w", encoding="utf-8") as f:
+                f.write(json.dumps(posts_copy))
+        except OSError:
+            print("Error writing to DB.")
+        else:
+            self.blog_posts = posts_copy
+            print(self.blog_posts)
+
+    def load_data(self) -> None:
+        """Load blog posts from a JSON file."""
+        try:
+            with Path("data/database.json").open(encoding="utf-8") as f:
+                self.blog_posts = json.load(f)
+        except FileNotFoundError:
+            print("Database file not found. Using empty blog posts.")
+            self.blog_posts = []
 
     def run(self, **kwargs: Any) -> None:  # noqa: ANN401
         """Start the Flask app."""
