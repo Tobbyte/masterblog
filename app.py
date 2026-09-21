0833b2824f478fbd5bc9f2bb267d7ab259e25dc1
Tobbyte <7722596+Tobbyte@users.noreply.github.com>
2026-09-21 10:15:50 +0200

    add delete and semi permanent uid saving

--- app.py
+++ app.py
@@ -21,7 +21,16 @@ class Masterblog:
             view_func=self.route_add,
             methods=["GET", "POST"],
         )
-        self.load_data()
+        self.app.add_url_rule(
+            "/delete/<int:post_id>",
+            view_func=self.route_delete,
+            methods=["GET", "POST"],
+        )
+        self.blog_posts = self.load_data()
+
+    def get_last_uid_from_posts(self) -> int:
+        """Cycles through all blog posts to get last id."""
+        return max(post["id"] for post in self.blog_posts)
 
     def get_uid(self) -> int:
         """Get a unique identifier for a new blog post.
@@ -37,51 +46,72 @@ class Masterblog:
         except FileNotFoundError:
             last_uid = ""
 
-        if not last_uid:
-            new_uid = len(self.blog_posts) + 1
-            with Path("data/uid").open("w", encoding="utf-8") as f:
-                f.write(str(new_uid))
-            return new_uid
-
         try:
-            return int(last_uid) + 1
+            new_uid = (
+                int(last_uid) + 1
+                if last_uid
+                else self.get_last_uid_from_posts() + 1
+            )
+            self.save_uid(new_uid)
         except ValueError:
             return 1
+        else:
+            return new_uid
+
+    def save_uid(self, new_uid: int) -> None:
+        with Path("data/uid").open("w", encoding="utf-8") as f:
+            f.write(str(new_uid))
+            print("newuid:", new_uid)
 
     def route_index(self) -> str:
         """Render the index page with blog posts."""
-        self.load_data()  # refresh
         return render_template(
             "index.html",
-            posts=self.blog_posts,
+            posts=self.load_data(),  # refresh
             blogtitle="Mein Blog",
         )
 
     def route_add(self) -> str | Response:
         """Render form on get or save post on post."""
         if request.method == "POST":
-            self.save_data(request.form.to_dict())  # use flat=False for multi
+            self.add_post(request.form.to_dict())  # use flat=False for multi
             return redirect(url_for("route_index"))
 
         return render_template("add.html")
 
-    def save_data(self, new_post: dict) -> None:
-        """Save a new blog post to the JSON file."""
+    def route_delete(self, post_id) -> Response:
+        print(f"delete: {post_id}")
+        if request.method == "POST":
+            self.del_post(post_id)  # use flat=False for multi
+
+        return redirect(url_for("route_index"))
+
+    def add_post(self, new_post):
         new_id = self.get_uid()
         new_post["id"] = new_id
         posts_copy = deepcopy(self.blog_posts)
         posts_copy.append(new_post)
+        self.save_data(posts_copy)
+
+    def del_post(self, post_id: int):
+        print("del:", post_id)
+        posts_copy = deepcopy(self.blog_posts)
+        posts = [post for post in posts_copy if post["id"] != post_id]
+        self.save_data(posts)
+
+    def save_data(self, blog_posts: list) -> None:
+        """Save a new blog post to the JSON file."""
         try:
             with Path("data/database.json").open("w", encoding="utf-8") as f:
-                f.write(json.dumps(posts_copy))
+                f.write(json.dumps(blog_posts))
         except OSError:
             print("Error writing to DB.")
         else:
             # update view only if crud went successful
-            self.blog_posts = posts_copy
+            self.blog_posts = blog_posts
             print(self.blog_posts)
 
-    def load_data(self) -> None:
+    def load_data(self) -> list:
         """Load blog posts from a JSON file."""
         try:
             with Path("data/database.json").open(encoding="utf-8") as f:
@@ -89,6 +119,7 @@ class Masterblog:
         except FileNotFoundError:
             print("Database file not found. Using empty blog posts.")
             self.blog_posts = []
+        return self.blog_posts
 
     def run(self, **kwargs: Any) -> None:  # noqa: ANN401
         """Start the Flask app."""
