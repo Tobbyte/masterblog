84d15277a8c90ef013639f5872d7f396ba58d6bc
Tobbyte <7722596+Tobbyte@users.noreply.github.com>
2026-09-21 18:07:55 +0200

    revert new id on update. makes no sense

--- app.py
+++ app.py
@@ -226,9 +226,7 @@ class Masterblog:
             posts_copy = deepcopy(self.blog_posts)
             new_post = request.form.to_dict()
             posts = [
-                {**post, **new_post, "id": self.get_uid()}
-                if post["id"] == post_id
-                else post
+                {**post, **new_post} if post["id"] == post_id else post
                 for post in posts_copy
             ]
             self.save_data(posts)
