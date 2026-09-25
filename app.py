083af7cd5d1f63d0c6efa28faf3cbbcdc79aeacd
Tobbyte <7722596+Tobbyte@users.noreply.github.com>
2026-09-25 12:46:36 +0200

    make helpers private, refactor order of code

--- app.py
+++ app.py
@@ -106,14 +106,14 @@ class Masterblog:
         """Render the 500 error page."""
         return render_template("500.html", error=error), 500
 
-    def get_last_uid_from_posts(self) -> int:
+    def _get_last_uid_from_posts(self) -> int:
         """Cycles through all blog posts to get last id."""
         posts = self.blog_store.load()
         if posts:
             return max(post["id"] for post in posts)
         return 0
 
-    def get_uid(self) -> int:
+    def _get_uid(self) -> int:
         """Get a unique identifier for a new blog post.
 
         Reads the last used UID from a file and increments it for
@@ -135,11 +135,11 @@ class Masterblog:
         try:
             new_uid = int(last_uid) + 1
         except ValueError:
-            new_uid = self.get_last_uid_from_posts() + 1
-        self.save_uid(new_uid)
+            new_uid = self._get_last_uid_from_posts() + 1
+        self._save_uid(new_uid)
         return new_uid
 
-    def save_uid(self, new_uid: int) -> None:
+    def _save_uid(self, new_uid: int) -> None:
         """Save the new UID to a file for future use."""
         try:
             with UID_FILE_PATH.open("w", encoding="utf-8") as f:
@@ -148,65 +148,13 @@ class Masterblog:
             print(ERR_SAVE_POST_UID)
             raise InternalServerError from e
 
-    def index(self) -> str:
-        """Render the index page with blog posts."""
-        posts = self.blog_store.load()
-        return render_template(
-            "index.html",
-            posts=posts,  # refresh
-            uuid=self.get_user_uid(),
-            blogtitle="Mein Blog",
-        )
-
-    def add(self) -> str | Response:
-        """Render form on get or save post on post."""
-        if request.method == "POST":
-            self.add_post(request.form.to_dict())  # use flat=False for multi
-            return redirect(url_for("index"))
-
-        return render_template("add.html")
-
-    def delete(self, post_id: int) -> Response:
-        """Delete blog post route."""
-        if self.fetch_post_by_id(post_id) is None:
-            abort(404)
-
-        if request.method == "POST":
-            self.del_post(post_id)
-
-        return redirect(url_for("index"))
-
-    def get_user_uid(self) -> str:
+    def _get_user_uid(self) -> str:
         """Get or create a user uid (for the current session)."""
         if "user_uid" not in session:
             session["user_uid"] = str(uuid.uuid4())
         return session["user_uid"]
 
-    def like_post(self, post_id: int) -> Response:
-        """Toggle like route."""
-        if self.fetch_post_by_id(post_id) is None:
-            abort(404)
-
-        user_uid = self.get_user_uid()
-        self.toggle_like(post_id, user_uid)
-        return redirect(url_for("index"))
-
-    def toggle_like(self, post_id: int, user_uid: str) -> None:
-        """Toggle the like status for a post by its ID."""
-        posts_copy = deepcopy(self.blog_store.load())
-
-        for post in posts_copy:
-            if post["id"] == post_id:
-                likes = post.setdefault("liked_by", [])
-                if user_uid in likes:
-                    likes.remove(user_uid)
-                else:
-                    likes.append(user_uid)
-                break
-
-        self.blog_store.save(posts_copy)
-
-    def fetch_post_by_id(self, post_id: int) -> dict | None:
+    def _fetch_post_by_id(self, post_id: int) -> dict | None:
         """Fetch a blog post from runtime data by its ID."""
         posts = self.blog_store.load()
         return next(
@@ -214,17 +162,72 @@ class Masterblog:
             None,
         )
 
+    ### route handlers ###
+
+    def index(self) -> str:
+        """Render the index page with blog posts."""
+        posts = self.blog_store.load()
+        return render_template(
+            "index.html",
+            posts=posts,  # refresh
+            uuid=self._get_user_uid(),
+            blogtitle="Mein Blog",
+        )
+
+    def add(self) -> str | Response:
+        """Render form on get or save post on post."""
+        if request.method == "POST":
+            self._add_post(request.form.to_dict())  # use flat=False for multi
+            return redirect(url_for("index"))
+
+        return render_template("add.html")
+
+    def delete(self, post_id: int) -> Response:
+        """Delete blog post route."""
+        if self._fetch_post_by_id(post_id) is None:
+            abort(404)
+
+        if request.method == "POST":
+            self._del_post(post_id)
+
+        return redirect(url_for("index"))
+
     def update_post(self, post_id: int) -> str | tuple | Response:
         """Update post route."""
-        post = self.fetch_post_by_id(post_id)
+        post = self._fetch_post_by_id(post_id)
         if post is None:
             abort(404)
         if request.method == "POST":
-            self.update_post_data(post_id, request.form.to_dict())
+            self._update_post_data(post_id, request.form.to_dict())
             return redirect(url_for("index"))
         return render_template("update.html", post=post)
 
-    def update_post_data(self, post_id: int, new_post_data: dict) -> None:
+    def like_post(self, post_id: int) -> Response:
+        """Toggle like route."""
+        if self._fetch_post_by_id(post_id) is None:
+            abort(404)
+
+        user_uid = self._get_user_uid()
+        self._toggle_like(post_id, user_uid)
+        return redirect(url_for("index"))
+
+    ### routes logic ###
+
+    def _add_post(self, new_post: dict) -> None:
+        """Add a new blog post."""
+        new_id = self._get_uid()
+        new_post["id"] = new_id
+        posts_copy = deepcopy(self.blog_store.load())
+        posts_copy.append(new_post)
+        self.blog_store.save(posts_copy)
+
+    def _del_post(self, post_id: int) -> None:
+        """Delete a blog post by its ID."""
+        posts_copy = deepcopy(self.blog_store.load())
+        posts = [post for post in posts_copy if post["id"] != post_id]
+        self.blog_store.save(posts)
+
+    def _update_post_data(self, post_id: int, new_post_data: dict) -> None:
         """Update the data of a blog post by its ID."""
         posts_copy = deepcopy(self.blog_store.load())
 
@@ -239,19 +242,22 @@ class Masterblog:
         ]
         self.blog_store.save(posts)
 
-    def add_post(self, new_post: dict) -> None:
-        """Add a new blog post."""
-        new_id = self.get_uid()
-        new_post["id"] = new_id
+    def _toggle_like(self, post_id: int, user_uid: str) -> None:
+        """Toggle the like status for a post by its ID."""
         posts_copy = deepcopy(self.blog_store.load())
-        posts_copy.append(new_post)
+
+        for post in posts_copy:
+            if post["id"] == post_id:
+                likes = post.setdefault("liked_by", [])
+                if user_uid in likes:
+                    likes.remove(user_uid)
+                else:
+                    likes.append(user_uid)
+                break
+
         self.blog_store.save(posts_copy)
 
-    def del_post(self, post_id: int) -> None:
-        """Delete a blog post by its ID."""
-        posts_copy = deepcopy(self.blog_store.load())
-        posts = [post for post in posts_copy if post["id"] != post_id]
-        self.blog_store.save(posts)
+    ###
 
     def run(self, **kwargs: Any) -> None:  # noqa: ANN401
         """Start the Flask app."""
